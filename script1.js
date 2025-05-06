42b0f3b1d04c46d7a42ec9aa99852f5f41bf6608
jennyo <jennifer.oleary.10@gmail.com>
2025-05-06 11:14:19 -0500

    edit & remove buttons appear, but don't function

--- script1.js
+++ script1.js
@@ -2,18 +2,27 @@ window.addEventListener("load", () => {
     const buttonAdd = document.getElementById("buttonAdd");
     const textInput = document.getElementById("newInput");
     const itemOutput = document.getElementById("output");
-    const buttonEdit = document.createElement("button");
-    const buttonRemove = document.createElement('button');
 
+    // addItem will create a list item that includes input text, an edit button, and a remove button for each item
     function addItem() {
+        // create list item with text input from html input field
         const newItem = document.createElement("li");
-        newItem.textContent = textInput.value;
+        newItem.textContent = textInput.value; // .value to fix [object HTMLInputElement]
         itemOutput.appendChild(newItem);
+
+        // edit button for each list item
+        const buttonEdit = document.createElement("button");
+        buttonEdit.textContent = "edit";
+        newItem.appendChild(buttonEdit);
+
+        // remove button for each list item    
+        const buttonRemove = document.createElement('button');
+        buttonRemove.textContent = "remove";
+        newItem.appendChild(buttonRemove);
     }
 
     buttonAdd.addEventListener("click", addItem);
-
-})
+});
 
 // const button = document.body.getElementById("add");
 // button.addEventListener("click", function () {
