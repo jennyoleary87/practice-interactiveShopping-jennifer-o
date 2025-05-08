52f9fb66a52c68cf1fd2c86ef541b83503d60cdb
jennyo <jennifer.oleary.10@gmail.com>
2025-05-07 21:10:51 -0500

    complete shopping list

--- script1.js
+++ script1.js
@@ -1,40 +1,60 @@
 window.addEventListener("load", () => {
     const buttonAdd = document.getElementById("buttonAdd");
     const textInput = document.getElementById("newInput");
-    const itemOutput = document.getElementById("output");
+    const list = document.getElementById("list");
 
     // addItem will create a list item that includes input text, an edit button, and a remove button for each item
     function addItem() {
         // create list item with text input from html input field
         const newItem = document.createElement("li");
         newItem.textContent = textInput.value; // .value to fix [object HTMLInputElement]
-        itemOutput.appendChild(newItem);
+        list.appendChild(newItem);
 
         // edit button for each list item
         const buttonEdit = document.createElement("button");
         buttonEdit.textContent = "edit";
         newItem.appendChild(buttonEdit);
+        buttonEdit.addEventListener("click", function () {
+            newItem.innerHTML = `<input type="text" value="" />
+            <button class="buttonSave">save</button>`; // removes edit and remove buttons & adds input field and save button
+            const editInput = newItem.querySelector("input");
+            const buttonSave = newItem.querySelector(".buttonSave");
+            buttonSave.addEventListener("click", function () {
+                const editedItem = editInput.value;
+                newItem.textContent = editedItem;
+
+                newItem.appendChild(buttonEdit);
+                newItem.appendChild(buttonRemove);
+                newItem = editedItem;
+            })
+        })
 
         // remove button for each list item    
         const buttonRemove = document.createElement('button');
         buttonRemove.textContent = "remove";
         newItem.appendChild(buttonRemove);
+        buttonRemove.addEventListener("click", function () {
+            newItem.remove();
+        });
     }
 
     buttonAdd.addEventListener("click", addItem);
+
 });
 
-// const button = document.body.getElementById("add");
-// button.addEventListener("click", function () {
-//     const input = document.body.getElementById("newInput");
-//     const output = document.body.createElement("li");
-//     output.textContent = "kiwi";
-//     document.body.appendChild(output);
-//     input.addEventListener("newInput", function (event) {
-//         output.textContent = `item: ${event.target.value}`;
-//     });
-// });
 
-// const element = document.body.getElementById("li");
-// element.remove();
+// const buttonEdit = document.createElement("button");
+// buttonEdit.textContent = "edit";
+// newItem.appendChild(buttonEdit);
+// buttonEdit.addEventListener("click", function () {
+//     newItem.innerHTML = `<input type="text" value="" />
+//     <button class="buttonSave">save</button>`; // removes edit and remove buttons & adds input field and save button
+//     const editInput = newItem.querySelector("input");
+//     const buttonSave = newItem.querySelector(".buttonSave");
+//     buttonSave.addEventListener("click", function () {
+//         const editedItem = editInput.value;
+//         newItem.textContent = editedItem;
 
+//         newItem.appendChild(buttonEdit);
+//         newItem.appendChild(buttonRemove);
+//         newItem = editedItem;
